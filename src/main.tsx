5000bb4b32b68d72c0c886cde0df1652b67bfefd
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-22 15:09:41 +0200

    [feat] : display PokemonCard on front

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
+import App from "./components/App.tsx";
+
+import "./styles/index.css";
 
 const rootElement = document.getElementById("root");
 
@@ -12,5 +14,5 @@ if (rootElement == null) {
 createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
